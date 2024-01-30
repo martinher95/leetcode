3b2399d78ab4ee72c3af9cee889cb54d92a497a7
Martin Hermes Anschau <martinhermes@weg.net>
2024-01-30 16:11:10 -0300

    ex118 updated

--- main4.py
+++ main4.py
@@ -2,3 +2,19 @@
 118. Pascal's Triangle
 """
 
+
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        triangle = [[1]]
+        for row in range(1, numRows):  # lines
+            line = [1]
+            if triangle:
+                for n in range(0, row - 1):
+                    line.append(triangle[row - 1][n] + triangle[row - 1][n + 1])
+                line.append(1)
+                triangle.append(line)
+        return triangle
